a3d49ffb488aaf4d93e4a0b6492055c6c4446844
Hans Kesting <hans_kesting@epam.com>
2019-08-15 12:13:21 +0200

    Started on the final level: personal details

--- src/main/java/nl/hdkesting/familyTree/ui/controllers/GeneaController.java
+++ src/main/java/nl/hdkesting/familyTree/ui/controllers/GeneaController.java
@@ -2,12 +2,15 @@ package nl.hdkesting.familyTree.ui.controllers;
 
 import nl.hdkesting.familyTree.core.dto.IndividualDto;
 import nl.hdkesting.familyTree.core.services.TreeService;
+import nl.hdkesting.familyTree.ui.viewModels.IndividualVm;
+import nl.hdkesting.familyTree.ui.viewModels.PersonVm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class GeneaController {
@@ -34,7 +37,16 @@ public class GeneaController {
 
     @GetMapping(path = "/person/{id}")
     public String getPerson(@PathVariable long id, Model model) {
+        PersonVm person = new PersonVm();
 
+        Optional<IndividualDto> opt = this.treeService.getIndividualById(id);
+        if (opt.isEmpty()) {
+            return "redirect:/familynames";
+        }
+
+        person.primary = new IndividualVm(opt.get());
+
+        model.addAttribute("person", person);
         return "person";
     }
 }
---
+++ src/main/java/nl/hdkesting/familyTree/ui/viewModels/IndividualVm.java
@@ -0,0 +1,39 @@
+package nl.hdkesting.familyTree.ui.viewModels;
+
+import nl.hdkesting.familyTree.core.dto.IndividualDto;
+import nl.hdkesting.familyTree.core.dto.Sex;
+
+import java.time.LocalDate;
+
+/**
+ * ViewModel with basic data for a single individual.
+ */
+public class IndividualVm {
+    public Long id;
+    public String firstNames;
+    public String lastName;
+    public char sex = ' '; // expected: 'M' or 'F'
+    public LocalDate birthDate;
+    public String birthPlace;
+    public LocalDate deathDate;
+    public String deathPlace;
+
+    public IndividualVm() {
+    }
+
+    public IndividualVm(IndividualDto dto) {
+        this.id = dto.getId();
+        this.firstNames = dto.getFirstNames();
+        this.lastName = dto.getLastName();
+        this.sex = dto.getSex() == Sex.Male ? 'M' :
+                   dto.getSex() == Sex.Female ? 'F' : ' ';
+        this.birthDate = dto.getBirthDate();
+        this.birthPlace = dto.getBirthPlace();
+        this.deathDate = dto.getDeathDate();
+        this.deathPlace = dto.getDeathPlace();
+    }
+
+    public String getFullName() {
+        return firstNames + " " + lastName;
+    }
+}
