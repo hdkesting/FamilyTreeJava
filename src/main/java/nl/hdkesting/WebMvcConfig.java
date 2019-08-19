e4a0bd60b7603f6ae53ed082529621eb1e0706fd
Hans Kesting <hans_kesting@epam.com>
2019-08-19 14:10:37 +0200

    fixed static files (but now needs /static/ path)

--- src/main/java/nl/hdkesting/WebMvcConfig.java
+++ src/main/java/nl/hdkesting/WebMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.session.MapSessionRepository;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -22,14 +23,21 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         return new MapSessionRepository(new ConcurrentHashMap<>());
     }
 
-    /**
-     * Handles favicon.ico requests assuring no <code>404 Not Found</code> error is returned.
-     */
-    @Controller
-    static class FaviconController {
-        @RequestMapping("favicon.ico")
-        String favicon() {
-            return "forward:/resources/static/favicon.ico";
-        }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
+
+//     /**
+//     * Handles favicon.ico requests assuring no <code>404 Not Found</code> error is returned.
+//     */
+//    @Controller
+//    static class FaviconController {
+//        @RequestMapping("favicon.ico")
+//        String favicon() {
+//            return "forward:/resources/static/favicon.ico";
+//        }
+//    }
 }
