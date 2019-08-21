f55bcd793bb765ca8fa6d38412ea862d27ee9aff
Hans Kesting <hans_kesting@epam.com>
2019-08-21 17:16:42 +0200

    set tab titles from within controller action

--- src/main/java/nl/hdkesting/familyTree/ui/controllers/DefaultController.java
+++ src/main/java/nl/hdkesting/familyTree/ui/controllers/DefaultController.java
@@ -1,13 +1,15 @@
 package nl.hdkesting.familyTree.ui.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DefaultController {
 
     @GetMapping(path = "/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("tabtitle", "Welcome");
         return "index";
     }
 }
--- src/main/java/nl/hdkesting/familyTree/ui/controllers/TestController.java
+++ src/main/java/nl/hdkesting/familyTree/ui/controllers/TestController.java
@@ -2,12 +2,9 @@ package nl.hdkesting.familyTree.ui.controllers;
 
 import nl.hdkesting.familyTree.core.dto.IndividualDto;
 import nl.hdkesting.familyTree.core.services.TreeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
