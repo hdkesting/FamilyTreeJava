c06ee2b2868f11cb5035f48d7841c3beb38a5b66
Hans Kesting <hans_kesting@epam.com>
2019-08-28 12:29:41 +0200

    sample REST PUT method added, support for multiple HTTP statuses as return

--- src/main/java/nl/hdkesting/familyTree/ui/controllers/api/PersonApiController.java
+++ src/main/java/nl/hdkesting/familyTree/ui/controllers/api/PersonApiController.java
@@ -1,36 +1,76 @@
 package nl.hdkesting.familyTree.ui.controllers.api;
 
 import nl.hdkesting.familyTree.core.dto.IndividualDto;
+import nl.hdkesting.familyTree.core.dto.Sex;
 import nl.hdkesting.familyTree.core.services.TreeService;
 import nl.hdkesting.familyTree.ui.viewModels.IndividualVm;
 import nl.hdkesting.familyTree.ui.viewModels.PersonDetailsVm;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping(path = "/api/")
 public class PersonApiController {
     private final TreeService treeService;
+    private static final String MIMETYPE_JSON = "application/json";
 
     public PersonApiController(TreeService treeService) {
         this.treeService = treeService;
     }
 
-    @GetMapping(path = "person/{id}")
-    public @ResponseBody IndividualVm getPerson(@PathVariable long id) {
+    @GetMapping(path = "person/{id}", produces = MIMETYPE_JSON)
+    public ResponseEntity<?> getPerson(@PathVariable long id) {
         PersonDetailsVm person = new PersonDetailsVm();
 
         Optional<IndividualDto> opt = this.treeService.getIndividualById(id);
         if (opt.isEmpty()) {
-            return null;
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         IndividualDto primary = opt.get();
-        return new IndividualVm(primary);
+        return new ResponseEntity<>(new IndividualVm(primary), HttpStatus.OK);
+    }
+
+    @PutMapping(path = "person/{id}", consumes = MIMETYPE_JSON)
+    public ResponseEntity<?> putPerson(@RequestBody IndividualVm individual, @PathVariable long id) {
+        if (individual == null || individual.getId() <= 0L || (individual.getFirstNames() == null && individual.getLastName() == null)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (individual.getId() != id) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Optional<IndividualDto> personOpt = this.treeService.getIndividualById(id);
+        if (personOpt.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        var person = personOpt.get();
+
+        person.setFirstNames(individual.getFirstNames());
+        person.setLastName(individual.getLastName());
+        person.setBirthDate(individual.getBirthDate());
+        person.setBirthPlace(individual.getBirthPlace());
+        person.setDeathDate(individual.getDeathDate());
+        person.setDeathPlace(individual.getDeathPlace());
+        switch (individual.getSex()) {
+            case 'M' :
+                person.setSex(Sex.Male);
+                break;
+            case 'F':
+                person.setSex(Sex.Female);
+                break;
+            default:
+                person.setSex(Sex.Unknown);
+                break;
+        }
+
+        this.treeService.update(person);
+
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
